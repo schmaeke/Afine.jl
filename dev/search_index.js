48afb19370b6edfdf75a0dfd3953f761ce925c10
Documenter.jl <documenter@juliadocs.github.io>
2024-08-19 20:01:22 +0000

    build based on 7c706c8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Hello-World","page":"Hello World","title":"Hello World","text":"","category":"section"},{"location":"","page":"Hello World","title":"Hello World","text":"unit_bounding_box( dimensions::Int64 )","category":"page"},{"location":"#Afine.Geometries.unit_bounding_box-Tuple{Int64}","page":"Hello World","title":"Afine.Geometries.unit_bounding_box","text":"unit_bounding_box( dimension )\n\nCreates a bounding box of 'dimension' with a centroid at the origin and all edges of length 2.\n\n\n\n\n\n","category":"method"}]
+}
