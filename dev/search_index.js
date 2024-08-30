3a55fdd4b0a1e166fafc25018b420b73275d0420
Documenter.jl <documenter@juliadocs.github.io>
2024-08-30 11:41:10 +0000

    build based on f3a22c0

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#Hello-World","page":"Hello World","title":"Hello World","text":"","category":"section"},{"location":"","page":"Hello World","title":"Hello World","text":"unit_bounding_box( dimensions::Int64 )","category":"page"},{"location":"#Afine.Geometries.unit_bounding_box-Tuple{Int64}","page":"Hello World","title":"Afine.Geometries.unit_bounding_box","text":"unit_bounding_box( dimension )\n\nCreates a bounding box of 'dimension' with a centroid at the origin and all edges of length 2.\n\n\n\n\n\n","category":"method"}]
+[{"location":"#The-documentation-of-Afine.jl","page":"The documentation of Afine.jl","title":"The documentation of Afine.jl","text":"","category":"section"},{"location":"","page":"The documentation of Afine.jl","title":"The documentation of Afine.jl","text":"There is nothing here at the moment. The documentation will be written when the package gets into the alpha stage.","category":"page"}]
 }
